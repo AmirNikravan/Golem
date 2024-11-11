dd125f6f23a68c23c60a65f5b115580d1cfd32e0
Amir <amirnikravan@outlook.com>
2024-11-11 11:39:51 +0330

    project statrted

---
+++ build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_installer_FILE D:/programming/installer/build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/installer.exe)
+set(__QT_DEPLOY_TARGET_installer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_installer_RUNTIME_DLLS D:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Core.dll)
